f99659bbdaf98e5e44299d57b758ae488734824e
homework Yaroslav Kireytsev <@>
2024-11-03 11:27:10 +0200

    не зовсім розумію

---
+++ Homework-1 Yaroslav Kireytsev.py
@@ -0,0 +1,9 @@
+#from random import randint
+#rand_value=randint(1,10)
+#print(rand_value)
+#user_input=print("Я загадав число від 1 до 10 спробуй його вгадати")
+#input("Веди своє число")
+#while input==rand_value: user_input=print("You won!")
+#Я не розумію чому воно не працює як треба
+#################################
+from random import randint
