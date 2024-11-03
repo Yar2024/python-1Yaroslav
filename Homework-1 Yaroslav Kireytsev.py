101e136aa02853843c9d55778ddac01a35499bd4
homework Yaroslav Kireytsev <@>
2024-11-03 14:15:09 +0200

    не зовсім розумію

--- Homework-1 Yaroslav Kireytsev.py
+++ Homework-1 Yaroslav Kireytsev.py
@@ -6,4 +6,21 @@
 #while input==rand_value: user_input=print("You won!")
 #Я не розумію чому воно не працює як треба
 #################################
-f
+
+#from random import randint
+#rand_value=randint(1,12)
+#months = {
+ #   1: "January",
+ # 3: "March",
+  ## 6: "June",
+   # 7: "July",
+  #  8: "August",
+  #  9: "September",
+  #  10: "October",
+  #  11: "November",
+  #  12: "December"
+}
+#random_months_number = randint(1, 12)
+##print(f"{random_months_number} - {month_name}") працює
+
+#останю задачу я не розумію
